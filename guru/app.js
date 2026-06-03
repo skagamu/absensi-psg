dede0d08ddcf701f5d7684e575de19596745848e
skagamu <pklgm25@gmail.com>
2026-06-03 13:51:12 +0700

    Update app.js

--- guru/app.js
+++ guru/app.js
@@ -1,4 +1,4 @@
-const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyZYfk70rs-WOOHQeq4RR93VtdzcpvTIk4aMv2rKUgFqGJ6RiOReb2QNnMbNZUp5fkLwg/exec";
+const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzJWdUPFWpuGEm6jY1cVsgUr-h1S9qAewQxDPxn3R9vkEQ9I8tnPaItJDchdt_TAE2blg/exec";
 
 // DOM Elements
 const sectionLogin = document.getElementById('loginSection');
